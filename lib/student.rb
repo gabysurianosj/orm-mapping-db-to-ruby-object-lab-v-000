f5632ad64f116b00ddeb3c2a4e18ff692ab6c623
Gabriela Suriano <surianogabriela@gmail.com>
2018-10-01 16:50:38 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -77,4 +77,11 @@ class Student
     first_X_students_in_grade_10(1)[0]
   end 
   
+  def self.all_students_in_grade_X(grade)
+    sql = "SELECT * FROM students WHERE grade = ?"
+    DB[:conn].execute(sql,grade).map do |return_array|
+      new_from_db(return_array)
+    end 
+  end 
+  
 end
