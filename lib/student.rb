2327365dee0ff01bb834139a65921945c23fce8d
Gabriela Suriano <surianogabriela@gmail.com>
2018-10-01 16:39:23 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,11 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    student = Student.new 
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student 
   end
 
   def self.all
@@ -13,6 +17,10 @@ class Student
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql = "SELECT * FROM students"
+    DB[:conn].execute(sql).map do |student_attributes|
+      new_from_db(student_attributes)
+    end 
   end
   
   def save
